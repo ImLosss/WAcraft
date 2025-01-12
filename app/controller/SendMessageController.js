edc2f3c52c330014eaa50a6dbdf4029fddac67fe
ImLosss <pahriansyahryan@gmail.com>
2025-01-12 22:11:36 +0800

    menambah pembaruan

--- app/controller/SendMessageController.js
+++ app/controller/SendMessageController.js
@@ -86,6 +86,54 @@ const sendMsgAll = withErrorHandling(async (msg, client, arg) => {
     });
 })
 
+const sendUpdate = withErrorHandling(async (msg, client) => {
+    const folderPath = 'database/data_user'; // Ganti dengan path menuju folder Anda
+    let update = fs.readFileSync(`update`, 'utf-8');
+    update = JSON.parse(update);
+
+    const keys = Object.keys(update);
+    const lastKey = keys[keys.length - 1];
+
+    update = update[lastKey].details;
+
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error('Error reading folder:', err);
+            return;
+        }
+
+        files.forEach(user => {
+            const filePath = path.join(folderPath, user);
+            
+            // Baca file JSON
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    console.error(`Error reading file ${user}:`, err);
+                    return;
+                }
+
+                // Ubah data dalam file JSON
+                try {
+                    const jsonData = JSON.parse(data);
+
+                    if(jsonData[0].ip) { 
+                        client.sendMessage(user, `${ update }`)
+                        .then(() => {
+                            msg.reply(`Success sending Message to ${ user }`);
+                        })
+                        .catch((e) => {
+                            console.log(e);
+                            msg.reply(`Error when sending Message to ${ user }`);
+                        })
+                    }
+                } catch (parseErr) {
+                    console.error(`Error when sending a Message:`, parseErr);
+                }
+            });
+        });
+    });
+});
+
 module.exports = {
-    sendMsg, sendMsgAll
+    sendMsg, sendMsgAll, sendUpdate
 }
