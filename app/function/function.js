0b4408d6f28af26040541c21cff9cd9b4bf95d74
ImLosss <pahriansyahryan@gmail.com>
2025-01-17 20:55:14 +0800

    update

--- app/function/function.js
+++ app/function/function.js
@@ -274,8 +274,8 @@ const withErrorHandling = (fn, defaultMsg, bot, defaultChat) => {
             if(bot) bot.quit();
 
             try {
-                if (msg) chat.sendMessage(`Terjadi kesalahan: ${err.message}`);
-                else if(chat) chat.sendMessage(`Terjadi kesalahan: ${err.message}`);
+                if(chat) chat.sendMessage(`Terjadi kesalahan: ${err.message}`);
+                else if (msg) chat.sendMessage(`Terjadi kesalahan: ${err.message}`);
                 else console.error(err);
             } catch (err) { console.error(err); }
         }
