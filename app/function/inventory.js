bc17458f4c31075d778aace946b943817b7c82f8
ImLosss <pahriansyahryan@gmail.com>
2025-02-26 12:16:24 +0800

    update timeout

--- app/function/inventory.js
+++ app/function/inventory.js
@@ -38,6 +38,43 @@ function getInventory(bot) {
     return strMsg;
 }
 
+function getInventoryByName(bot, name) {
+
+    let items = bot.inventory.slots.filter((item) => { return item != undefined });
+
+    if(items.length == 0) return 'Inventory kosong';
+
+    let no = 0;
+    let strMsg = '*_slot_ | _name_ | _count_*\n';
+
+    items.forEach((item) => {
+
+        console.log(item);
+        try {
+            if (item.nbt == null || item.nbt.value.display == undefined) {
+                let name = item.displayName;
+                strMsg += `\n${ no++ } | ${ name } | ${ item.count }`;
+            } else {
+                let name = item.nbt.value.display.value.Name.value;
+                name = JSON.parse(name);
+                if (name.extra == undefined) name = name.text;
+                else if (name.extra[0].extra == undefined) name = name.extra[0].text;
+                else name = name.extra[0].extra.map(item => item.text).join('');
+                strMsg += `\n${ no++ } | ${ name } | ${ item.count }`;
+            }
+        } catch (err) {
+            no++;
+            console.error('Error ketika mengambil inventory: ' . err);
+            console.error(JSON.stringify(item));
+            console.error(item.nbt.value.display);
+        }
+    });
+
+    strMsg += `\n\n_Kirim */throw <slot>* untuk buang item yang anda mau. Contoh: /throw 0_\n\nKirim */equip <slot> (destination)* untuk menggunakan item yang ada di inventory. Contoh: /equip 0 torso\n*Destination*\n- hand\n- head\n- torso\n- legs\n- feet\n\n*_Note:_*\n_Selalu kirim /inventory untuk cek slot anda sebelum membuang item agar item yang anda buang tidak salah_`;
+
+    return;
+}
+
 function throwItem(bot, msg, arg) {
     try {
 
@@ -130,5 +167,5 @@ function equipItem(bot, msg, arg) {
 }
 
 module.exports = {
-    getInventory, throwItem, equipItem
+    getInventory, throwItem, equipItem, getInventoryByName
 }
